d2d8f26907e876542880276f5b45cc6099237c8e
Allan <allangeek@gmail.com>
2022-03-30 14:48:26 -0300

    Adiciona logica para cadastrar usuario no DB

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { GetStaticProps } from 'next';
 import { SubscribeButton } from '../components/SubscribeButton';
 
 import styles from './home.module.scss';
-import { stripe } from '../../services/stripe';
+import { stripe } from '../services/stripe';
 
 interface HomeProps {
   product: {

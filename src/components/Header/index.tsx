360e7187bdead6be4f1e85c7858c7d4d9c0d8882
Allan <allangeek@gmail.com>
2022-04-04 11:59:31 -0300

    Cria component ActiveLink substituindo o Linkna navegacao

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import Link from 'next/link';
+import { ActiveLink } from '../ActiveLink';
 import { SignInButton } from '../SignInButton';
 import styles from './styles.module.scss';
 
@@ -8,13 +8,13 @@ export function Header() {
       <div className={styles.headerContent}>
         <img src='/images/logo.svg' alt='ig.news' />
         <nav>
-          <Link href='/'>
-            <a className={styles.active}>Home</a>
-          </Link>
+          <ActiveLink activeClassName={styles.active} href='/'>
+            <a>Home</a>
+          </ActiveLink>
 
-          <Link href='/posts'>
+          <ActiveLink activeClassName={styles.active} href='/posts'>
             <a>Posts</a>
-          </Link>
+          </ActiveLink>
         </nav>
 
         <SignInButton />
